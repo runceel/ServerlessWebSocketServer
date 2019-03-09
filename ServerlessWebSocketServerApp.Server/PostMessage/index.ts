411e5df82b5b2d57c26854ff24435b243f2a20b6
Kazuki Ota <kaota@microsoft.com>
2019-03-09 15:42:38 +0900

    add test project

--- ServerlessWebSocketServerApp.Server/PostMessage/index.ts
+++ ServerlessWebSocketServerApp.Server/PostMessage/index.ts
@@ -2,9 +2,9 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { SignalRMessage } from "../signalr/signalrmessage"
 
 const httpTrigger: AzureFunction = async function (context: Context, 
-    req: HttpRequest, 
-    signalRMessages: SignalRMessage[]): Promise<void> {
-    signalRMessages.push({
+    req: HttpRequest): Promise<void> {
+    context.log(JSON.stringify(req.body));
+    context.bindings.signalRMessages = new Array<SignalRMessage>({
         target: "receiveMessage",
         arguments: [
             req.body,
--- signalr-vue-client/src/components/HelloWorld.vue
+++ signalr-vue-client/src/components/HelloWorld.vue
@@ -1,57 +1,69 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div>
+      <input type="text" v-model="message" />
+      <button @click="onPostMessageClick">PostMessage</button>
+    </div>
+    <div>
+      <div :key="index" v-for="(chatMessage, index) in chatMessages">
+        {{ chatMessage }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { HubConnection, HubConnectionBuilder, LogLevel, JsonHubProtocol } from '@aspnet/signalr';
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  public message: string = '';
+  public chatMessages: string[] = [];
+
+  private connection!: HubConnection;
+
+  public async onPostMessageClick(): Promise<void> {
+    if (this.connection == null) {
+      return;
+    }
+
+    await fetch('http://localhost:7071/api/PostMessage', {
+      method: 'POST',
+      body: JSON.stringify({ text: this.message }),
+    });
+
+    this.message = '';
+  }
+
+  public async created(): Promise<void> {
+    console.log('created called');
+    this.connection = new HubConnectionBuilder()
+      .withUrl('http://localhost:7071/api')
+      .configureLogging(LogLevel.Information)
+      .build();
+    this.connection.on('receiveMessage', (message) => {
+      console.log(JSON.stringify(message));
+      this.chatMessages.push(message.text);
+    });
+    this.connection.onclose = this.connect;
+    await this.connect();
+    console.log('created finished: ' + this.connection);
+  }
+
+  private async connect(): Promise<void> {
+    while(true) {
+      try {
+        await this.connection.start();
+        break;
+      } catch (e) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      }
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
